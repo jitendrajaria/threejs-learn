dcea1a0cbf5c3e6febe9e6c01491154093869177
jitendra.jaria <jitendrajaria96@gmail.com>
2023-12-07 01:07:54 +0530

    Adding Basic scene in three js

---
+++ src/script.js
@@ -0,0 +1,35 @@
+import * as THREE from "three";
+
+const canvas = document.querySelector("canvas.webgl");
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+
+scene.add(camera);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  camera.aspect = sizes.width / sizes.height;
+  renderer.setSize(sizes.width, sizes.height);
+
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
